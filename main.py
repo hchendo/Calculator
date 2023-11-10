e7538b632df0693b020d0ac7f94e6a22d7abe1ed
hchendo <hchendo@gmail.com>
2023-11-10 15:56:37 -0800

    create calc class with add func

---
+++ main.py
@@ -0,0 +1,10 @@
+class SimpleCalculator:
+    """
+    A simple calculator class for basic arithmetic operations.
+    """
+
+    def add(self, a, b):
+        """
+        Returns the sum of a and b.
+        """
+        return a + b
