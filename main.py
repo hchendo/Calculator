23d6e7ef2eefcad67f9413c88acdfc8833cb63a6
hchendo <hchendo@gmail.com>
2023-11-17 16:21:15 -0800

    Calculator with add, subtract, multiply, and divide functions

--- main.py
+++ main.py
@@ -8,3 +8,23 @@ class SimpleCalculator:
         Returns the sum of a and b.
         """
         return a + b
+
+    def subtract(self, a, b):
+        """
+        Returns the difference of a and b.
+        """
+        return a - b
+
+    def multiply(self, a, b):
+        """
+        Returns the product of a and b.
+        """
+        return a * b
+
+    def divide(self, a, b):
+        """
+        Returns the division of a by b.
+        """
+        if b == 0:
+            raise ValueError("Cannot divide by zero.")
+        return a / b
