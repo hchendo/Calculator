c762c7560c0f00a264f9a68c896505ee58fcbe0d
hchendo <hchendo@gmail.com>
2023-12-01 15:01:15 -0800

    slight bug fixes

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 def add(numbers):
     """
-    Returns the sum of a and b.
+    Returns the sum of numbers[0] and numbers[1].
     """
     return numbers[0] + numbers[1]
 
 def subtract(numbers):
     """
-    Returns the difference of a and b.
+    Returns the difference of numbers[0] and numbers[1].
     """
     return numbers[0] - numbers[1]
 
 def multiply(numbers):
     """
-    Returns the product of a and b.
+    Returns the product of numbers[0] and numbers[1].
     """
     return numbers[0] * numbers[1]
 
 def divide(numbers):
     """
-    Returns the division of a by b.
+    Returns the division of numbers[0] and numbers[1].
     """
-    if b == 0:
+    if numbers[1] == 0:
         raise ValueError("Cannot divide by zero.")
     return numbers[0] / numbers[1]
