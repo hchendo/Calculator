4e754d264015dfbe1e6af177ef0c05c3e53dd884
hchendo <hchendo@gmail.com>
2023-12-01 13:42:30 -0800

    Removed simple calculator class, functions now defined outside of any class

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
-class SimpleCalculator:
+
+
+def add(self, a, b):
     """
-    A simple calculator class for basic arithmetic operations.
+    Returns the sum of a and b.
     """
+    return a + b
 
-    def add(self, a, b):
-        """
-        Returns the sum of a and b.
-        """
-        return a + b
+def subtract(self, a, b):
+    """
+    Returns the difference of a and b.
+    """
+    return a - b
 
-    def subtract(self, a, b):
-        """
-        Returns the difference of a and b.
-        """
-        return a - b
+def multiply(self, a, b):
+    """
+    Returns the product of a and b.
+    """
+    return a * b
 
-    def multiply(self, a, b):
-        """
-        Returns the product of a and b.
-        """
-        return a * b
-
-    def divide(self, a, b):
-        """
-        Returns the division of a by b.
-        """
-        if b == 0:
-            raise ValueError("Cannot divide by zero.")
-        return a / b
+def divide(self, a, b):
+    """
+    Returns the division of a by b.
+    """
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
